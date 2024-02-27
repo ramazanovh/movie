eeaf98b6cfcd25fc67a628f6df96fa06b0bad45d
ramazanovh <pasterx39@gmail.com>
2024-02-27 21:07:29 +0400

    fetch api

--- js/movie.js
+++ js/movie.js
@@ -2,3 +2,9 @@ const movieID = new URLSearchParams(location.search).get("id");
 const detailPage = document.querySelector(".detailPage");
 let api = "https://api.tvmaze.com/shows";
 let details;
+fetch(api + "/" + movieID)
+  .then((res) => res.json())
+  .then((data) => {
+    renderDetails(data);
+    console.log(data);
+  });
