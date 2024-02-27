6d2041177d874bd70d20b585076f5f75cbe748e1
ramazanovh <pasterx39@gmail.com>
2024-02-27 21:00:36 +0400

    created js and variables

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let inputEl = document.querySelector(".input-el");
+let inputBtn = document.querySelector(".inputBtn");
+let cards = document.querySelector(".cards");
+let allmovies;
+let api = "https://api.tvmaze.com/shows";
+let paginationCount = 0;
